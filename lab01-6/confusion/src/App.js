9ab8afe2e6c0a1e52b1285ac4521865b2beddeec
anhminh171299 <anhminh171299@gmail.com>
2021-10-27 09:44:53 +0700

    commit

--- lab01-6/confusion/src/App.js
+++ lab01-6/confusion/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import Menu from './components/MenuComponent';
 
 function App() {
     return ( <
@@ -13,6 +14,8 @@ function App() {
         NavbarBrand href = "/" > Ristorante Con Fusion < /NavbarBrand> <
         /div> <
         /Navbar> <
+        Menu / >
+        <
         /div>
     );
 }
