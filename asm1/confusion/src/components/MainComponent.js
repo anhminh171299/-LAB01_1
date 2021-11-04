79090c18a7e4e6a17b0a1ccc185eae62c66a3c4d
anhminh171299 <anhminh171299@gmail.com>
2021-11-04 16:40:08 +0700

    commit

--- asm1/confusion/src/components/MainComponent.js
+++ asm1/confusion/src/components/MainComponent.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
 import Home from "./HomeComponent";
 import Menu from "./MenuComponent";
+import Contact from "./ContactComponent";
 import DishDetail from "./DishDetailComponent";
+import { COMMENTS } from "../shared/comments";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import { DISHES } from "../shared/dishes";
@@ -13,12 +17,30 @@ class Main extends Component {
     this.state = {
       dishes: DISHES,
       selectedDish: null,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
     };
   }
 
   render() {
     const HomePage = () => {
-      return <Home />;
+      return (
+        <Home
+          dish={
+            this.state.dishes &&
+            this.state.dishes.filter((dish) => dish.featured)[0]
+          }
+          promotion={
+            this.state.promotions &&
+            this.state.promotions.filter((promo) => promo.featured)[0]
+          }
+          leader={
+            this.state.leaders &&
+            this.state.leaders.filter((leader) => leader.featured)[0]
+          }
+        />
+      );
     };
     return (
       <div>
@@ -30,6 +52,7 @@ class Main extends Component {
             path="/menu"
             component={() => <Menu dishes={this.state.dishes} />}
           />
+          <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
