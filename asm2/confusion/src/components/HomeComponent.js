9b22cbf4068649392ad944efcf8921a410096636
anhminh171299 <anhminh171299@gmail.com>
2021-11-23 16:33:21 +0700

    lab06-1

--- asm2/confusion/src/components/HomeComponent.js
+++ asm2/confusion/src/components/HomeComponent.js
@@ -1,37 +1,39 @@
-import React, { useState } from "react";
-// import { DEPARTMENTS, ROLE, STAFFS } from '../shared/staffs';
-import dateFormat from "dateformat";
-import "./staff.css";
-import { Card,} from "reactstrap";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle} from 'reactstrap';
 
-function RenderStaff({ staff }) {
-  return (
-    <div className="col-xs-6 col-md-4 col-lg-2">
-      <div className="staff-name">
+function RenderCard({item}) {
+
+    return(
         <Card>
-          <Link to={`/staff/${staff.id}`}>
-            <img src={staff.image} />
-            <p>{staff.name}</p>
-          </Link>
+            {item && item.image && <CardImg src={item.image} alt={item.name} />}
+            <CardBody>
+            {item&&<CardTitle>{item.name}</CardTitle>}
+            {item && item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null }
+            {/* <CardText>{item.description}</CardText> */}
+            </CardBody>
         </Card>
-      </div>
-    </div>
-  );
+    );
+
 }
 
-function StaffList(props) {
-  
-  return (
-    <div className="container">
-      <h3>Nhân viên</h3>
-      <hr />
-      <div className="row">
-        {props.staffs.map((staff) => (
-          <RenderStaff key={staff.id} staff={staff} />
-        ))}
-      </div>
-    </div>
-  );
+function Home(props) {
+    console.log(props)
+    return(
+        <div className="container">
+            <div className="row align-items-start">
+                <div className="col-12 col-md m-1">
+                    <RenderCard item={props.dish} />
+                </div>
+                <div className="col-12 col-md m-1">
+                    <RenderCard item={props.promotion} />
+                </div>
+                <div className="col-12 col-md m-1">
+                    <RenderCard item={props.leader} />
+                </div>
+            </div>
+        </div>
+    );
 }
-export default StaffList;
+
+export default Home;
--- asm2/confusion/src/components/MainComponent.js
+++ asm2/confusion/src/components/MainComponent.js
@@ -1,68 +1,79 @@
-import React, { Component } from 'react';
-import Home from './HomeComponent';
-import StaffList from './HomeComponent';
-import DetailStaff from './DetailStaff';
-import Contact from './ContactComponent';
-import About from './AboutComponent';
+import React, { Component } from "react";
+import Home from "./HomeComponent";
+import Menu from "./MenuComponent";
+import Contact from "./ContactComponent";
 import DishDetail from "./DishDetailComponent";
-import Header from './HeaderComponent';
-import Footer from './FooterComponent';
-import Menu from './MenuComponent';
-import { DEPARTMENTS, ROLE, STAFFS } from "../shared/staffs";
-import { Switch, Route, Redirect } from 'react-router-dom'
+import { COMMENTS } from "../shared/comments";
+import { PROMOTIONS } from "../shared/promotions";
+import { LEADERS } from "../shared/leaders";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
+import { DISHES } from "../shared/dishes";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 class Main extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            staffs: STAFFS,
-            departments: DEPARTMENTS
-        }
-    }
-    render() {
-        const HomePage = () => {
-            return (
-                <Home staffs={this.state.staffs}
-                    
-                />
-            )
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+      selectedDish: null,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
+    };
+  }
 
-        const AboutUs = () => {
-            return (
-                <About  departments={this.state.departments} />
-            )
-        }
-        const Contact = () => {
-            return (
-                <Menu  staffs={this.state.staffs} />
-            )
-        }
-
-        const staffwithId = ({ match }) => {
-            return (
-                <DetailStaff   staff={this.state.staffs.filter((staff) =>
-                    staff.id === parseInt(match.params.staffId, 10))[0]}
-                />
-            )
-
-        }
-
-        return (
-            <div>
-                <Header />
-                <Switch>
-                    <Route exact path='/staff' component={HomePage} />
-                    <Route exact path='/staff' component={() => <StaffList staffs={this.state.staffs} />} />
-                    <Route path='/staff/:staffId' component={staffwithId} />
-                    <Route path='/menu' component={Contact} />
-                    <Route path='/aboutus' component={AboutUs} />
-                    <Redirect to='/staff' />
-                </Switch>
-                <Footer />
-            </div>
-        )
-    }
+  render() {
+    const HomePage = () => {
+      return (
+        <Home
+          dish={
+            this.state.dishes &&
+            this.state.dishes.filter((dish) => dish.featured)[0]
+          }
+          promotion={
+            this.state.promotions &&
+            this.state.promotions.filter((promo) => promo.featured)[0]
+          }
+          leader={
+            this.state.leaders &&
+            this.state.leaders.filter((leader) => leader.featured)[0]
+          }
+        />
+      );
+    };
+    const DishWithId = ({ match }) => {
+      return (
+        <DishDetail
+          dish={
+            this.state.dishes.filter(
+              (dish) => dish.id === parseInt(match.params.dishId, 10)
+            )[0]
+          }
+          comments={this.state.comments.filter(
+            (comment) => comment.dishId === parseInt(match.params.dishId, 10)
+          )}
+        />
+      );
+    };
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route
+            exact
+            path="/menu"
+            component={() => <Menu dishes={this.state.dishes} />}
+          />
+          <Route path='/menu/:dishId' component={DishWithId} />
+          <Route exact path="/contactus" component={Contact} />
+          <Redirect to="/home" />
+        </Switch>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default Main;
