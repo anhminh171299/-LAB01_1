1a61948fea716ac3a56dfb89027f5261d4be6d9d
anhminh171299 <anhminh171299@gmail.com>
2021-12-20 22:42:27 +0700

    lab09-1

--- asm3/confusion/src/components/DishDetailComponent.js
+++ asm3/confusion/src/components/DishDetailComponent.js
@@ -10,6 +10,7 @@ import {
 } from "reactstrap";
 import { Link } from "react-router-dom";
 import CommentForm from "./CommentForm";
+import { Loading } from '../redux/LoadingComponent';
 
 function RenderDish({ dish }) {
   return (
@@ -55,7 +56,25 @@ function RenderComments({ comments, addComment, dishId }) {
 }
 
 const DishDetail = (props) => {
-  console.log({props})
+  if (props.isLoading) {
+    return(
+        <div className="container">
+            <div className="row">            
+                <Loading />
+            </div>
+        </div>
+    );
+}
+else if (props.errMess) {
+    return(
+        <div className="container">
+            <div className="row">            
+                <h4>{props.errMess}</h4>
+            </div>
+        </div>
+    );
+}
+else if (props.dish != null) 
   if (props.dish != null)
     return (
       <div className="container">
